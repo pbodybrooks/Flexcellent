dcc5b15edafbdc1332ea5adfe9d3728167c7bee4
Parker <parkerlamarbrook@gmail.com>
2023-05-23 21:27:40 -0600

    added additional starter files and code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,4 @@
+// THIS IS JUST COPIED CODE FROM THE ACTIVITIES
 const router = require('express').Router();
 const { User } = require('../../models');
 
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS fitness_db;
+
+CREATE DATABASE fitness_db;
