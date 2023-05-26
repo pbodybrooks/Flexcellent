12f9f74ff1b2aecadfb36fd381c7537f119eab6b
Ash Reid <ashley.reid013@gmail.com>
2023-05-26 17:55:33 -0600

    file name changes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+// TODO: will need routes for: login, home, myworkouts, explore
+// TODO: might need routes for: home/rewards
+
+module.exports = router;
