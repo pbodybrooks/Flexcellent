6ef11359ffb48ba657e0be95ac058263ac612bbc
Ash Reid <ashley.reid013@gmail.com>
2023-05-26 21:23:03 -0600

    adds some homeRoutes configs and a withAuth helper

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-
-const userRoutes = require('./user-routes');
+const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
-// TODO: will need routes for: login, home, myworkouts, explore
-// TODO: might need routes for: home/rewards
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const userInput = await User.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['name', 'ASC']],
+        });
+
+        const users = userInput.map((project) => project.get({ plain: true }));
+
+        res.render('home', {
+            users, 
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
 
 module.exports = router;
+
+// TODO: will need routes for: login, home, myworkouts, explore
+// TODO: might need routes for: home/rewards
