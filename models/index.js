78be67b0596e1ee35da648206812e14cbd373b18
Parker <parkerlamarbrook@gmail.com>
2023-05-27 16:49:15 -0600

    added Exercise and Workout routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,20 @@
 const User = require('./User');
+const Exercise = require('./Exercise');
+const Workout = require('./Workout');
 
-module.exports = { User };
+Workout.hasMany(Exercise, {
+    foreignKey: 'workout_id',
+    onDelete: 'CASCADE'
+});
+
+Exercise.belongsTo(Workout, {
+    foreignKey: 'workout_id'
+});
+
+// TO DO: Make sure this is correct
+User.hasMany(Workout, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Exercise, Workout}
