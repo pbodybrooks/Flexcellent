b380ae8abd1d89929510c19ea421eafb0bab3b1a
Ash Reid <ashley.reid013@gmail.com>
2023-05-30 18:09:44 -0600

    rebase

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Workout } = require('../models');
+const { User, Workout, Exercise } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
@@ -49,14 +49,32 @@ router.get('/register', (req, res) => {
 
 router.get('/explore', withAuth, async (req, res) => {
     try {
-        const userData = await User.findByPk(req.session.user_id, {
-            attributes: { exclude: ['password'] },
-            include: [{ model: Workout }],
-        });
+        // const userInput = await User.findByPk(req.session.user_id, {
+        //     attributes: { exclude: ['password'] },
+        //     include: [{ model: Workout }],
+        // });
 
         // const user = userData.get({ plain: true });
 
         res.render('explore', {
+            // ...user,
+            // logged_in: true
+        });    
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/myWorkouts', withAuth, async (req, res) => {
+    try {
+        // const userInput = await User.findByPk(req.session.user_id, {
+        //     attributes: { exclude: ['password'] },
+        //     include: [{ model: Workout }],
+        // });
+
+        // const user = userInput.get({ plain: true });
+
+        res.render('workouts', {
             // ...user,
             // logged_in: true
         });    
