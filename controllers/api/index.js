3d1a91b008beec6732daa51638eb02fc5cf3b99d
Ash Reid <ashley.reid013@gmail.com>
2023-05-30 18:09:44 -0600

    rebase

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const workoutRoutes = require('./workoutRoutes');
+const exerciseRoutes = require('./exerciseRoutes');
 
 router.use('/users', userRoutes);
+router.use('/workouts', workoutRoutes);
+router.use('/exercises', exerciseRoutes);
 
 module.exports = router;
