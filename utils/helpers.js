d65182674f73a8452ecf7d46b39f80ddff983aa5
Parker <parkerlamarbrook@gmail.com>
2023-06-01 19:12:49 -0600

    added toTitleCase handlebars helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,4 +3,13 @@ module.exports = {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
     },
+    toTitleCase: (str) => {
+      let words = str.replace("_", " ").split(" ");
+    let titleCaseWords = words.map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    let titleCaseStr = titleCaseWords.join(" ");
+    return titleCaseStr;
+    },      
 };
+
