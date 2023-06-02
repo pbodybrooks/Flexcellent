1a165751f49015459fc14128217f1ced639e4c53
mkidee <mckaymemmott@gmail.com>
2023-06-01 20:37:53 -0600

    created a button that will create a new workout card each time you press it

--- public/js/workouts.js
+++ public/js/workouts.js
@@ -1,1 +1,16 @@
-const workBtn = document.getElementsByClassName('workbtn');
+
+
+// function formAppear() {
+//     document.getElementById("workForm").style.visibility = "visible";
+// }
+
+// document.getElementsById("workbtn").addEventListener("click", formAppear);
+
+function createCard() {
+    var cardEle = document.createElement('div');
+    var container = document.querySelector('.workouts');
+    cardEle.style.width = '250px';
+    cardEle.style.height = '400px';
+    cardEle.style.backgroundColor = 'lightgrey';
+    container.appendChild(cardEle);
+}
