8d104a318922a2bbf085d9dc19709c066301450d
mkidee <mckaymemmott@gmail.com>
2023-06-02 17:28:02 -0600

    moved some js styling into css for simplicity. working on a way to add form into all cards

--- public/js/workouts.js
+++ public/js/workouts.js
@@ -8,9 +8,37 @@
 
 function createCard() {
     var cardEle = document.createElement('div');
+    cardEle.classList.add('workCard');
     var container = document.querySelector('.workouts');
-    cardEle.style.width = '250px';
-    cardEle.style.height = '400px';
-    cardEle.style.backgroundColor = 'lightgrey';
+
+    // cardEle.style.width = '250px';
+    // cardEle.style.height = '400px';
+    // cardEle.style.backgroundColor = 'lightgrey';
+    // cardEle.style.boxShadow = '7px 7px 5px grey';
+    // cardEle.style.margin = '12px';
+    // cardEle.style.borderRadius = '10px';
+
     container.appendChild(cardEle);
+
+    $(document).ready
+        (
+        function() {
+                $("<div class='form-holder'>" +
+            "<form name='input' action='#' method='get'>" +
+            "<label for='exercise'>Exercise</label>" +
+            "<input type='text' id='exercise' name='exercise'><br><br>" +
+            "<label for='sets'>Sets</label>" +
+            "<input type='text' id='sets' name='sets' ><br>" +
+            "<label for='reps'>Reps</label>" +
+            "<input type='text' id='reps' name='reps'><br>" +
+            "<label for='weight'>Weight</label>" +
+            "<input type='text' id='weight' name='w eight'><br><br>" +
+            "<input type='submit' value='Save'>" +
+            "</form>" +
+            "</div>").appendTo('.workCard');
+        }
+    )
 }
+
+
+
