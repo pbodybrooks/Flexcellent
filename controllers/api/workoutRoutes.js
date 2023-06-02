8761490e1d22d1bc7adc41b012a49c2f56b7f807
Ash Reid <ashley.reid013@gmail.com>
2023-06-01 20:49:21 -0600

    adds workout and measurment routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,10 +1,19 @@
 //TODO: include workouts, exercises, measurements, explore... maybe?
 const router = require('express').Router();
-const { User, Workout } = require('../../models');
+const { Workout } = require('../../models');
 
 router.post('/myWorkouts', async (req, res) => {
     try {
-        
+        const userInput = await Workout.create({
+            description: req.body.description,
+            date_created: req.body.date_created
+        });
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+            req.session.user_id = userInput.user_id;
+            res.status(200).json(userInput);
+        });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
