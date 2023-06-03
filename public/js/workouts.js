756558b07216b5d570aea3b30ead931a337f384c
mkidee <mckaymemmott@gmail.com>
2023-06-02 18:17:12 -0600

    got the form to show up within the cards

--- public/js/workouts.js
+++ public/js/workouts.js
@@ -9,7 +9,10 @@
 function createCard() {
     var cardEle = document.createElement('div');
     cardEle.classList.add('workCard');
+
     var container = document.querySelector('.workouts');
+
+    console.log("clicked");
 
     // cardEle.style.width = '250px';
     // cardEle.style.height = '400px';
@@ -20,24 +23,55 @@ function createCard() {
 
     container.appendChild(cardEle);
 
-    $(document).ready
-        (
-        function() {
-                $("<div class='form-holder'>" +
-            "<form name='input' action='#' method='get'>" +
-            "<label for='exercise'>Exercise</label>" +
-            "<input type='text' id='exercise' name='exercise'><br><br>" +
-            "<label for='sets'>Sets</label>" +
-            "<input type='text' id='sets' name='sets' ><br>" +
-            "<label for='reps'>Reps</label>" +
-            "<input type='text' id='reps' name='reps'><br>" +
-            "<label for='weight'>Weight</label>" +
-            "<input type='text' id='weight' name='w eight'><br><br>" +
-            "<input type='submit' value='Save'>" +
-            "</form>" +
-            "</div>").appendTo('.workCard');
-        }
-    )
+    const nodeList = document.querySelectorAll(".workCard");
+    console.log(nodeList);
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].innerHTML ="<form name='input' action='#' method='get'>" +
+        "<label for='exercise'>Exercise</label>" +
+        "<input type='text' id='exercise' name='exercise'><br><br>" +
+        "<label for='sets'>Sets</label>" +
+        "<input type='text' id='sets' name='sets' ><br>" +
+        "<label for='reps'>Reps</label>" +
+        "<input type='text' id='reps' name='reps'><br>" +
+        "<label for='weight'>Weight</label>" +
+        "<input type='text' id='weight' name='w eight'><br><br>" +
+        "<input type='submit' value='Save'>" +
+        "</form>"
+        ;
+    }
+
+    // document.querySelectorAll(".workCard").innerHTML=
+    // "<form name='input' action='#' method='get'>" +
+    // "<label for='exercise'>Exercise</label>" +
+    // "<input type='text' id='exercise' name='exercise'><br><br>" +
+    // "<label for='sets'>Sets</label>" +
+    // "<input type='text' id='sets' name='sets' ><br>" +
+    // "<label for='reps'>Reps</label>" +
+    // "<input type='text' id='reps' name='reps'><br>" +
+    // "<label for='weight'>Weight</label>" +
+    // "<input type='text' id='weight' name='w eight'><br><br>" +
+    // "<input type='submit' value='Save'>" +
+    // "</form>"
+    // ;
+
+    // $(document).ready
+    //     (
+    //     function() {
+    //             $("<div class='form-holder'>" +
+    //         "<form name='input' action='#' method='get'>" +
+    //         "<label for='exercise'>Exercise</label>" +
+    //         "<input type='text' id='exercise' name='exercise'><br><br>" +
+    //         "<label for='sets'>Sets</label>" +
+    //         "<input type='text' id='sets' name='sets' ><br>" +
+    //         "<label for='reps'>Reps</label>" +
+    //         "<input type='text' id='reps' name='reps'><br>" +
+    //         "<label for='weight'>Weight</label>" +
+    //         "<input type='text' id='weight' name='w eight'><br><br>" +
+    //         "<input type='submit' value='Save'>" +
+    //         "</form>" +
+    //         "</div>").appendTo('.workCard');
+    //     }
+    // )
 }
 
 
