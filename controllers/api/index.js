d5c87c268dd3aef766aaf55ec0744d3e448a2302
Ash Reid <ashley.reid013@gmail.com>
2023-06-03 18:24:16 -0600

    adds post requests and deletes measureRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,10 @@ const userRoutes = require('./userRoutes');
 const workoutRoutes = require('./workoutRoutes');
 const exerciseRoutes = require('./exerciseRoutes');
 const weightRoutes = require('./trackerRoutes');
-const measureRoutes = require('./measureRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/workouts', workoutRoutes);
-// router.use('/measurements', measureRoutes);
 router.use('/exercises', exerciseRoutes);
 router.use('/weight', weightRoutes);
 
