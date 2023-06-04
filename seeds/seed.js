fd9e10a7065c7bf6e2d7050144d0853904f54385
Parker <parkerlamarbrook@gmail.com>
2023-06-03 18:08:16 -0600

    added seed weightData.json

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,18 +3,25 @@ const { User, Exercise, Workout } = require('../models');
 // const seedFILE2 = require('');
 
 const userData = require('./userData.json');
+const exerciseData = require('./exerciseData.json');
+const workoutData = require('./workoutData.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   
-    // ADD FILES TO BE SEEDED - needs updates
-    const users = await User.bulkCreate(userData, {
+    // WORK IN PROGRESS
+    await User.bulkCreate(userData, {
       individualHooks: true,
       returning: true,
     });
-    //await seedSEEDFILE1();
-    //await seedSEEDFILE2();
-
+    await Workout.bulkCreate(workoutData, {
+      individualHooks: true,
+      returning: true,
+    });
+    await Exercise.bulkCreate(exerciseData, {
+      individualHooks: true,
+      returning: true,
+    });
   process.exit(0);
 };
 
