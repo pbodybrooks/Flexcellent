b6ba0a37ab3cf21ce305ece049a3467deb842242
Parker <parkerlamarbrook@gmail.com>
2023-06-04 16:39:59 -0600

    removed old format_date helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,4 @@
 module.exports = {
-    // format_date: (date) => {
-    //   // Format date as MM/DD/YYYY
-    //   return date.toLocaleDateString();
-    // },
     // format date as MM/DD/YYYY
     format_date: (date) => {
       return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
@@ -17,7 +13,7 @@ module.exports = {
     let titleCaseStr = titleCaseWords.join(" ");
     return titleCaseStr;
     }, 
-    
+
     // simple helper to add two numbers used to format indexes from 0 to 1, 1 to 2, etc.
     addHelper : (a, b) => {
       return a + b;
