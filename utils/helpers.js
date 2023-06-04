95d25c4d9c72f33cc2abad91054b91d566ddff42
Parker <parkerlamarbrook@gmail.com>
2023-06-04 17:38:58 -0600

    accidentally deleted toTitleCase helper, re-added

--- utils/helpers.js
+++ utils/helpers.js
@@ -30,6 +30,16 @@ module.exports = {
     
       return formattedDate.replace(/\b\d+\b/, (match) => match + suffix);
     },
+
+    // format words in title case, replace underscores with spaces
+    toTitleCase: (str) => {
+      let words = str.replace("_", " ").split(" ");
+      let titleCaseWords = words.map(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      });
+      let titleCaseStr = titleCaseWords.join(" ");
+      return titleCaseStr;
+    }, 
         
     // simple helper to add two numbers used to format indexes from 0 to 1, 1 to 2, etc.
     addHelper : (a, b) => {
