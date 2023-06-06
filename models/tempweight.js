0b3b4b87340906a11344ae3df522f9b40844cabc
Ash Reid <ashley.reid013@gmail.com>
2023-06-06 00:00:27 -0600

    rename weight

---
+++ models/tempweight.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Weight extends Model {}
+
+Weight.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    weight: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'weight',
+});
+
+module.exports = Weight;
