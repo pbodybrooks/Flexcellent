d4707aa25be60efa506a82c8668a9c7eb9deccc3
CaptainFlopp3y <barrancofabian@gmail.com>
2023-06-06 08:19:32 -0600

    updating chart and fixing bugs

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,10 +4,21 @@ const trackerBtn = document.getElementById('weightTrackerButton');
 const yearElement = document.getElementById("year");
 const weight = document.getElementById("barvalue");
 const weekly = document.getElementById("trackerDate");
+const month = document.getElementById("month");
 let chart;
 
+function getMinMax() {
+    const monthDate = month.value
+    const yearDate = yearElement.value
+    const min = `${yearDate}-${(monthDate) < 10 ? "0" + (monthDate) : monthDate}`;
+    const lastDate = new Date(yearDate, monthDate, 1)
+    const max = `${lastDate.getFullYear()}-${(lastDate.getMonth() + 1) < 10 ? "0" + (lastDate.getMonth() + 1) : lastDate.getMonth()}`
+    return { min: min, max: max }
+
+}
+
 function chart2() {
-    const year = yearElement.value
+    const minMax = getMinMax()
     const plugin = {
         id: 'customCanvasBackgroundColor',
         beforeDraw: (chart, args, options) => {
@@ -47,8 +58,8 @@ function chart2() {
                     time: {
                         unit: 'month'
                     },
-                    min: `${year}-01`,
-                    max: `${year}-12`
+                    min: minMax.min,
+                    max: minMax.max
 
                 }
             }
@@ -67,7 +78,6 @@ async function deleteWeight(id) {
 async function upWeight(id) {
     const weight = document.querySelector(`.row${id} input.weight`).value
     const date = document.querySelector(`.row${id} input.date`).value
-    console.log(weight)
     const remove = await fetch('/api/weight/update', {
         method: "put", headers: {
             "Content-Type": "application/json",
@@ -79,13 +89,17 @@ async function upWeight(id) {
 async function userData() {
     const response = await fetch('/api/weight/retriever');
     const jsonData = await response.json();
+    console.log(jsonData)
     chart.data.datasets[0].data = jsonData.weights.map((value) => { return { x: value.x, y: value.y } })
     chart.update();
     const weightDataElement = document.getElementById("weightdata")
     let html = ""
     for (let i = 0; i < jsonData.weights.length; i++) {
         const date = new Date(jsonData.weights[i].x);
-        const formattedDated = `${date.getFullYear()}-${(date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : date.getMonth()}-${(date.getDate() + 1) < 10 ? "0" + (date.getDate() + 1) : date.getDate()}`;
+
+        const formattedDated = `${date.getFullYear()}-${(date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1}-${(date.getDate() < 10 ? "0" + date.getDate() : date.getDate())}`;
+
+        console.log(date, formattedDated)
         html += `<div class= "row${jsonData.weights[i].id}">
         <input class="weight" type="number" value= "${jsonData.weights[i].y}"/>
     <input class="date" type="date" value = "${formattedDated}"/>
@@ -97,6 +111,7 @@ async function userData() {
     </div>`
     }
     weightDataElement.innerHTML = html;
+
 }
 function init() {
     yearElement.value = new Date().getFullYear()
@@ -122,8 +137,15 @@ trackerBtn.addEventListener('click', async () => {
     // chart.update();
 });
 yearElement.addEventListener('change', () => {
-    const year = yearElement.value
-    chart.options.scales.x.min = `${year}-01`
-    chart.options.scales.x.max = `${year}-12`
+    const minMax = getMinMax()
+    chart.options.scales.x.min = minMax.min
+    chart.options.scales.x.max = minMax.max
+    chart.update();
+});
+
+month.addEventListener('change', () => {
+    const minMax = getMinMax()
+    chart.options.scales.x.min = minMax.min
+    chart.options.scales.x.max = minMax.max
     chart.update();
 });
